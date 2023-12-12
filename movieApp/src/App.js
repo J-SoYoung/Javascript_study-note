d2069b5a3212bf87330198b0535a7f40e9a2a23a
thdud <thgml1444@naver.com>
2023-12-13 08:18:00 +0900

    class 내부에서 map, filter메서드 연달아 사용하기

--- movieApp/src/App.js
+++ movieApp/src/App.js
@@ -12,15 +12,24 @@ export default class App extends Component {
     super({
       state : { 
         fruits:[ 
-          {name:"Apple",price:1000},
-          {name:"Apple2",price:2000},
-          {name:"Apple3",price:3000},
-          {name:"Apple4",price:4000},
+          {name:"Apple", price:1000},
+          {name:"Apple2", price:2000},
+          {name:"Apple3", price:3000},
+          {name:"Apple4", price:4000},
         ]
       }
     })
   }
   render(){
     console.log(this.state.fruits)
+    this.el.innerHTML=/*html */`
+    <h1>fruits</h1>
+    <ul>
+      ${this.state.fruits
+        .filter((fruit)=> fruit.price < 3000)
+        .map((fruit)=>`<li>${fruit.name}</li>` )
+        .join('')}
+    </ul>
+    `
   }
 }
