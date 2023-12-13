b40f35d108779aeeca94d3ea98fad6b5f7745525
soyoung <thgml1444@naver.com>
2023-12-13 15:48:13 +0900

    TheHeader컴포넌트 생성, router-view라는 태그를 생성-> 해쉬 라우터 이동시 생성될 페이지가 넣을 공간 만들어줌

--- movieApp/src/App.js
+++ movieApp/src/App.js
@@ -1,40 +1,13 @@
-// export default class App{
-//   constructor(){
-//     this.el = document.createElement('div')
-//     this.el.textContent = 'hellod'
-//   }
-// }
-
 import { Component } from './core/heropy.js'
-import FruitItem from './components/FruitItem.js'
+import TheHeader from './components/TheHeader.js'
 
 export default class App extends Component {
-  constructor(){
-    super({
-      state : { 
-        fruits:[ 
-          {name:"Apple", price:1000},
-          {name:"Apple2", price:2000},
-          {name:"Apple3", price:3000},
-          {name:"Apple4", price:4000},
-        ]
-      }
-    })
-  }
+
   render(){
-    console.log(this.state.fruits)
-    this.el.innerHTML=/*html */`
-    <h1>fruits</h1>
-    <ul></ul>
-    `
-    const ulEl = this.el.querySelector('ul')
-    ulEl.append(...this.state.fruits
-        .map((fruit)=> new FruitItem({
-          props: {
-            name:fruit.name,
-            price:fruit.price
-          }
-        }).el )
+    const routerVIew = document.createElement('router-view')
+    this.el.append( 
+      new TheHeader().el,
+      routerVIew
     )
   }
 }
