982a2c90ececaffb170ae2135fb5d7855024484c
soyoung <thgml1444@naver.com>
2023-12-13 16:06:59 +0900

    App에 router실행함수 작성

--- movieApp/src/main.js
+++ movieApp/src/main.js
@@ -1,7 +1,11 @@
 import App from './App.js'
+// 가져오기 이름은 router, 원래 함수이름은 createRouter
+import router from './routes/index.js';
 
 const root = document.querySelector('#root')
 // 클래스 객체 생성 -> 
 root.append(new App().el);
 
-console.log('시작')
+// 라우터 실행
+router()
+
